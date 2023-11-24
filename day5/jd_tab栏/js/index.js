f73f3d345239cd6e0f9f9fba19e41b6a72c05318
xxxxx-3747 <1921708164@qq.com>
2023-11-24 19:31:31 +0800

    京东

---
+++ day5/jd_tab栏/js/index.js
@@ -0,0 +1,11 @@
+//获取相关元素
+var message=$('message');
+
+var message_menu=$('message_menu');
+
+var menu_l = $('menu_l');
+var menu_l_a = menu_l.querySelectorAll('a');
+
+var message_content=$('message_content');
+
+
